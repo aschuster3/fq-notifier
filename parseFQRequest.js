505cd8f10fa818e43c0003da82d5e32ff55d3ace
Andrew Schuster <aschuster93@gmail.com>
2019-01-06 16:16:24 -0500

    Remove unused file

--- parseFQRequest.js
+++
@@ -1,8 +0,0 @@
-const request = require('request');
-
-let requestParser = function(request_id, action) {
-  if(action !== 'create') return 'Do nothing';
-  return 'Do something';
-}
-
-module.exports.parseFQRequest = requestParser
